04a8e87bfcc89920f9df5ac609c830cf89b13996
Milad <emintowpac@gmail.com>
2021-08-02 13:59:49 +0300

    Added visual markup of main elements

---
+++ app.js
@@ -0,0 +1,26 @@
+const containerDiv = document.querySelector('.container');
+
+function createGrid (gridNumber) { 
+    let gridArea = gridNumber * gridNumber;
+    for (let i = 1; i <= gridArea; i++) {
+
+        const gridItem = document.createElement('div');
+        containerDiv.style.gridTemplateColumns = `repeat(${gridNumber}, 3rem)`;
+        containerDiv.style.gridTemplateRows = `repeat(${gridNumber}, 3rem)`;
+        gridItem.style.padding = "5%";
+        gridItem.classList.add('gridcell');
+        containerDiv.appendChild(gridItem);
+    } 
+    
+}
+
+
+createGrid(10);
+
+const allCells = document.querySelectorAll(`.gridcell`);
+
+allCells.forEach(cell => {
+    cell.addEventListener('mouseover', function(){
+        cell.style.backgroundColor = "black";
+    })
+});
